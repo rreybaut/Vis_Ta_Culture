9cff194fbafc5cdd1066f59b50f3355cc6e9f075
root <karine.egler@gmail.com>
2022-11-22 15:53:18 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,15 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Callback environment..."
-#User.destroy_all
+Offer.destroy_all
+User.destroy_all
 
-#User.create!(email: "karine.egler@gmail.com", password:"123456", first_name: "Karine", last_name: "Egler",buyer: false, seller: true)
-#User.create!(email: "valerian.pallas@gmail.com", password:"123456", first_name: "Valeria", last_name: "Pallas",buyer: false, seller: true)
-#User.create!(email: "	rreybaut@gmail.com", password:"123456", first_name: "Raphael", last_name: "Reybaut",buyer: false, seller: true)
+user1 = User.create!(email: "karine.egler@gmail.com", password:"123456", first_name: "Karine", last_name: "Egler",buyer: false, seller: true)
+user2 = User.create!(email: "valerian.pallas@gmail.com", password:"123456", first_name: "Valeria", last_name: "Pallas",buyer: false, seller: true)
+user3 = User.create!(email: "	rreybaut@gmail.com", password:"123456", first_name: "Raphael", last_name: "Reybaut",buyer: false, seller: true)
 
 Offer.destroy_all
 
-Offer.create!(country: "Argentine", city: "Buenos Aires",price: "2500.0",lodging: true, meal: true, pet: false,smoker: false, excursion: true,child: true,user_id: 8)
-Offer.create!(country: "Irlande",city: "Dublin", price: "900.0", lodging: true, meal: true, pet: false, smoker: false, excursion: true,child: true, user_id:9)
-Offer.create!(country: "Maroc",city: "Casablanca", price: "1500.0", lodging: true, meal: true, pet: false, smoker: false, excursion: true, child: true, user_id:10)
+Offer.create!(country: "Argentine", city: "Buenos Aires",price: "2500.0",lodging: true, meal: true, pet: false,smoker: false, excursion: true,child: true,user_id: user1.id)
+Offer.create!(country: "Irlande",city: "Dublin", price: "900.0", lodging: true, meal: true, pet: false, smoker: false, excursion: true,child: true, user_id: user2.id)
+Offer.create!(country: "Maroc",city: "Casablanca", price: "1500.0", lodging: true, meal: true, pet: false, smoker: false, excursion: true, child: true, user_id: user3.id)
