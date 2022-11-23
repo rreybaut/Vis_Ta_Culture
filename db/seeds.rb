70752bdc48241a1f26333702c04b49c7ee19d642
valeria pallas <valerianpallas@gmail.com>
2022-11-23 15:23:57 +0100

    cards INDEX donne

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,6 @@ user3 = User.create!(email: "	rreybaut@gmail.com", password:"123456", first_name
 Offer.create!(country: "Argentine", city: "Buenos Aires",price: "2500.0",lodging: true, meal: true, pet: false,smoker: false, excursion: true,child: true,user_id: user1.id)
 Offer.create!(country: "Irlande",city: "Dublin", price: "900.0", lodging: true, meal: true, pet: false, smoker: false, excursion: true,child: true, user_id: user2.id)
 Offer.create!(country: "Maroc",city: "Casablanca", price: "1500.0", lodging: true, meal: true, pet: false, smoker: false, excursion: true, child: true, user_id: user3.id)
+Offer.create!(country: "France", city: "Paris",price: "2500.0",lodging: true, meal: true, pet: false, smoker: true, excursion: true, child: false, user_id: user1.id)
+Offer.create!(country: "Senegal",city: "Dakar", price: "3050.0", lodging: true, meal: true, pet: true, smoker: true, excursion: true, child: true, user_id: user2.id)
+Offer.create!(country: "USA",city: "New York", price: "4500.0", lodging: true, meal: true, pet: false, smoker: false, excursion: true, child: false, user_id: user3.id)
