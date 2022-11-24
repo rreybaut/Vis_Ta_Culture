7a957cbfb224ff24cdf953703d419f003eeea7dd
raphael reybaut <rreybaut@gmail.com>
2022-11-24 16:50:17 +0100

    new html booking controller new and create done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,25 @@
 class BookingsController < ApplicationController
   def new
-
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new
   end
 
   def create
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new(booking_params)
+    @booking.offer = @offer
+    @booking.user = current_user
 
+    if @booking.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :comment, :booked)
   end
 end
